4b8b3c3a2528652672c8c37ce37bcf57d38bc419
vdagranovich <the.goat@mail.ru>
2025-11-28 19:11:32 +0300

    All of the tests have passed

--- raindrops.py
+++ raindrops.py
@@ -1,18 +1,23 @@
-def convert(n, res):
-    if n % 3 == 0 and res != 'Pling':
+def convert(number):
+    res = ''
+    if number % 3 == 0 and res != 'Pling':
         res += 'Pling'
-        n = n // 3
-        convert(n, res)
+        number = number // 3
+        convert(number)
 
-    if n % 5 == 0 and res != 'Plang':
+    if number % 5 == 0 and res != 'Plang':
         res += 'Plang'
-        n = n // 5
-        convert(n, res)
+        number = number // 5
+        convert(number)
 
-    if n % 7 == 0 and res != 'Plong':
+    if number % 7 == 0 and res != 'Plong':
         res += 'Plong'
-        n = n // 7
-        convert(n, res)
-    return res
+        number = number // 7
+        convert(number)
+    else:
+        if res == '':
+            return str(number)
+    return str(res)
+    
 
-print(convert(int(input()), ''))
+print(convert(int(input())))
