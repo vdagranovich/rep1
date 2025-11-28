edd1640d99735fdced85d680e71fb33fff0a3cd1
vdagranovich <the.goat@mail.ru>
2025-11-28 18:08:19 +0300

    fh2

--- raindrops.py
+++ raindrops.py
@@ -1,19 +1,18 @@
-# n = int(input())
-def gett(n, res):
+def convert(n, res):
     if n % 3 == 0 and res != 'Pling':
         res += 'Pling'
         n = n // 3
-        gett(n, res)
+        convert(n, res)
 
     if n % 5 == 0 and res != 'Plang':
         res += 'Plang'
         n = n // 5
-        gett(n, res)
+        convert(n, res)
 
     if n % 7 == 0 and res != 'Plong':
         res += 'Plong'
         n = n // 7
-        gett(n, res)
+        convert(n, res)
     return res
 
-print(gett(int(input()), ''))
+print(convert(int(input()), ''))
